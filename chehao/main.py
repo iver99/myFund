c369e1a423791cdbdddffc7b267b767aeb578391
chehao <chendong.h.hao@oracle.com>
2017-11-16 16:53:00 +0800

    add log module

--- chehao/main.py
+++ chehao/main.py
@@ -1,35 +1,34 @@
 # -*- coding: utf-8 -*-
 import urllib.request, socket, re, sys, os
+import logging
+import logging.config
 from urllib import request
 from bs4 import BeautifulSoup as bs
-
+# import fundutil
 # 保存首页的图片到本地
 # 定义文件保存路径
 targetPath = "d:\\myfund.txt"
+logging.config.fileConfig('util/logging.conf')
 
-def getFundInfo(map):
+def getFundInfoRecentMonth(map, result):
     # iterate each fund info
     for key, value in map.items():
-        fund_key = key
         fund_resp = request.urlopen(value)
         fund_data_html = fund_resp.read().decode('utf-8')
         soup = bs(fund_data_html, 'html.parser')
-        num_ui = soup.find_all('span', class_=['ui-font-middle','ui-num'])
+        num_ui = soup.find_all('span', class_=['ui-font-middle', 'ui-num'])
         for item in num_ui:
             try:
-                if item.find_previous('span').get_text().index('近1月') >=0:
-                    print(key)
-                    print('近一月: ', item.string)
+                if item.find_previous('span').get_text().index('近1月') >= 0:
+                    # print(key)
+                    # print(item.get_text()[:-1])
+                    # print(int(item.get_text()[:-1]))
+                    # remove the at tail'%'
+                    result[key] = float(item.get_text()[:-1])
                 else:
                     print(item.find_previous('span').string)
             except ValueError:
                 continue
-
-            # print(item.string)
-        # tmp = soup.find(text = "近1月").find_parent().find_next_sibling()
-        # print(tmp.string)
-        # print(key)
-        # print(num_ui[1].string)
 
 
 def getFundList(map, url):
@@ -37,20 +36,15 @@ def getFundList(map, url):
     html_data = resp.read().decode('gbk')
     soup = bs(html_data, 'html.parser')
     num_boxes = soup.find_all('div', class_='num_box')
-    # TODO: handle fund start with '0' first
-    fund_list = num_boxes[0].find_all('li')
+    # TODO: 暂时先处理基金代号7开头的基金
+    fund_list = num_boxes[7].find_all('li')
     for item in fund_list:
         # 有三个<a>,只要第一个, ex:（092002）大成债券C | 基金吧 | 档案
         specific_fund = item.find_all('a')
-        # fileHandle.write(specific_fund[0]['href'] + '\n')
-        # fileHandle.write(specific_fund[0].string + '\n')
         if len(specific_fund):
-            # print(specific_fund[0]['href'])
-            # print(specific_fund[0].string)
             map[specific_fund[0].string] = specific_fund[0]['href']
         else:
             print("*Fund list is empty!!!*")
-
 
 
 url = "http://fund.eastmoney.com/allfund.html"
@@ -60,13 +54,14 @@ headers = {
 
 
 def main():
-    # fundMap = {}
-    # for debug
-    fundMap = {}
-    getFundList(fundMap, url)
-    getFundInfo(fundMap)
+    logger = logging.getLogger('main')
+    logger.info("Main function begin...")
+    fund_map = {}
+    getFundList(fund_map, url)
+    recent_month = {}
+    getFundInfoRecentMonth(fund_map, recent_month)
 
-    print(fundMap)
+    print(recent_month)
 
-
+    logger.info("Main Function end...")
 main()
