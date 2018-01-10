a3f6d1b5eca31457428af0c2353addb8c2545732
chehao <chendong.h.hao@oracle.com>
2018-01-10 13:28:59 +0800

    提高爬基金效率

--- fund/main.py
+++ fund/main.py
@@ -20,38 +20,36 @@ def getFundInfoRecentMonth(map, normal_fund, currency_fund_map, bond_fund_map):
         fund_resp = request.urlopen(value)
         fund_data_html = fund_resp.read().decode('utf-8')
         soup = bs(fund_data_html, 'html.parser')
-        num_ui = soup.find_all('span', class_=['ui-font-middle', 'ui-num'])
-        for item in num_ui:
-            #FIXME 尝试能否优化下面的搜索效率
-            if item.find_previous('span').get_text().count('近1月') > 0:
-                # 基金页面可能存在认购页面，导致无法解析出最近一月的收益，catch住这个ValueError然后继续
-                try:
-                    fund_recent_month = float(item.get_text()[:-1])
-                except ValueError as e:
-                    logger.warn(e)
-                    logger.warn("基金代码是 %s " % key)
-                    continue
-                # 暂时丢弃最近一个月收益为小于 1% 的基金，提高效率
-                if fund_recent_month < 1.0:
-                    continue
-                else:
-                    if key.count('ETF基金') > 0:
-                        currency_fund_map[key] = fund_recent_month
-                        logger.debug("ETF基金: fund key is [%s] and value is [%s] " % (key, item.get_text()[:-1]))
-                    elif key.count('债券') > 0:
-                        bond_fund_map[key] = fund_recent_month
-                        logger.debug("债券基金: fund key is [%s] and value is [%s] " % (key, item.get_text()[:-1]))
-                    else:
-                        normal_fund[key] = fund_recent_month
-                        logger.debug("普通基金: fund key is [%s] and value is [%s] " % (key, item.get_text()[:-1]))
-     # FIXME fix below bug of time cost
+        num_ui = soup.find_all('span', class_=['ui-font-middle'], limit=3)
+        # 基金页面可能存在认购页面，导致无法解析出最近一月的收益，catch住这个ValueError然后继续
+        try:
+            # 最后一个值是最近一个月的收益
+            fund_recent_month = float(num_ui[2].get_text()[:-1])
+        except ValueError as e:
+            logger.warn(e)
+            logger.warn("基金代码是 %s " % key)
+            continue
+        # 暂时丢弃最近一个月收益为小于 1% 的基金，提高效率
+        if fund_recent_month < 1.0:
+            continue
+        else:
+            if key.count('ETF基金') > 0:
+                currency_fund_map[key] = fund_recent_month
+                logger.debug("ETF基金: fund key is [%s] and value is [%s] " % (key, num_ui[2].get_text()[:-1]))
+            elif key.count('债券') > 0:
+                bond_fund_map[key] = fund_recent_month
+                logger.debug("债券基金: fund key is [%s] and value is [%s] " % (key, num_ui[2].get_text()[:-1]))
+            else:
+                normal_fund[key] = fund_recent_month
+                logger.debug("普通基金: fund key is [%s] and value is [%s] " % (key, num_ui[2].get_text()[:-1]))
+                # FIXME fix below bug of time cost
     logger.info("#2 current time is %d" % time.time())
     logger.info("获取基金最近一月信息结束...%s took %d ms" % (threading.current_thread().name, (time.time() - current_time)))
 
 
 # index is fund code start number ex: 方正富邦货币B(730103), index is 7
 def getFundList(map, url, index):
-    start_time  = time.time();
+    start_time = time.time();
     logger.info("开始获取基金列表,基金开头数字为 " + str(index))
     logger.debug("访问 url:" + url)
     resp = request.urlopen(url)
@@ -64,7 +62,8 @@ def getFundList(map, url, index):
         # 有三个<a>,只要第一个, ex:<a>（092002）大成债券C</a> | <a>基金吧</a> | <a>档案</a>
         specific_fund = item.find('a')
         # 丢弃行为放到这个会效率比较高, 丢弃货币基金
-        if specific_fund == None or specific_fund.string.count('货币') or specific_fund.string.count('现金'):
+        if specific_fund == None or specific_fund.string.count('货币') or specific_fund.string.count(
+                '现金') or specific_fund.string.count('保本'):
             continue
         map[specific_fund.string] = specific_fund['href']
     logger.info("结束处理开头为%d的基金，页面获取基金个数为 %d" % (index, len(map)))
@@ -72,6 +71,8 @@ def getFundList(map, url, index):
 
 
 fund_url = "http://fund.eastmoney.com/allfund.html"
+
+
 # headers = {
 #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '                            'Chrome/51.0.2704.63 Safari/537.36'
 # }
